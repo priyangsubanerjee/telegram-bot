fe566292f7183ad2cef01234b4e7af788edb35b0
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-03-01 19:40:09 +0530

    text

--- index.js
+++ index.js
@@ -10,34 +10,14 @@ bot.command("start", (ctx) => {
   );
 });
 
-bot.hears("animals", (ctx) => {
-  console.log(ctx.from);
-  let animalMessage = `great, here are pictures of animals you would love`;
-  ctx.deleteMessage();
-  bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "dog",
-            callback_data: "dog",
-          },
-          {
-            text: "cat",
-            callback_data: "cat",
-          },
-        ],
-      ],
-    },
-  });
-});
+// listen for text messages
 
-bot.hears("phone", (ctx, next) => {
-  console.log(ctx.from);
+bot.on("text", (ctx) => {
+  console.log(ctx.message.text);
   bot.telegram.sendMessage(
     ctx.chat.id,
-    "Can we get access to your phone number?",
-    requestPhoneKeyboard
+    "hello there! Welcome to my new telegram bot.",
+    {}
   );
 });
 
@@ -58,3 +38,8 @@ const requestPhoneKeyboard = {
 };
 
 bot.launch();
+
+// https://good-blue-fish-tux.cyclic.app
+// 6005874620:AAE2ilM3xpnglJg70OmhG6JFsB9yvxx214Y
+
+// https://api.telegram.org/bot6005874620:AAE2ilM3xpnglJg70OmhG6JFsB9yvxx214Y/setWebhook?url=https://good-blue-fish-tux.cyclic.app
